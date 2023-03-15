8f3b4d870571057679dfdf6cd4db0c1dcc77bc42
Osama <0xm.osamaa@gmail.com>
2023-03-15 23:01:24 +0200

    borrows router 🫳

---
+++ Routes/BorrowsRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../Controllers/BorrowsController");
+const validator = require("../Core/ValidationMW/BorrowsValidation");
+const auth = require("../Core/AuthenticationMw/Authorization");
+
+router
+    .route("/borrows")
+    .get(auth.employeeOrAbove, controller.getAllBorrows)
+    .post(auth.employeeOrAbove, validator.addValidator, controller.addBorrow);
+
+router
+    .route("/borrows/:_id")
+    .put(auth.employeeOrAbove, validator.editValidator, controller.updateBorrow)
+    .delete(auth.employeeOrAbove, validator.deleteValidator, controller.deleteBorrow);
+
+module.exports = router;
