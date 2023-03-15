54e73490067c563f1a79618b964f530dc97a4ab5
Osama <0xm.osamaa@gmail.com>
2023-03-15 22:34:08 +0200

    borrows model 🫳

---
+++ Model/BorrowsModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const borrowsSchema = mongoose.Schema({
+    bookID: {
+        type: String,
+        trim: true,
+        required: [true, "Enter a valid book ID"],
+    },
+    memberID: {
+        type: String,
+        trim: true,
+        required: [true, "Enter a valid book ID"],
+    },
+    employeeID: {
+        type: String,
+        trim: true,
+        required: [true, "Enter a valid book ID"],
+    },
+    borrowDate: {
+        type: Date,
+        required: [true, "Enter a valid borrow date"],
+        default: Date.now,
+    },
+    returnDate: {
+        type: Date,
+        default: null,
+        required: false,
+    },
+    deadlineDate: {
+        type: Date,
+        required: [true, "Enter deadline date"],
+    },
+});
+
+mongoose.model("borrows", borrowsSchema);
