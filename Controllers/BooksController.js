ef8eef3907de9560f033c4832245196cf9f4c4b0
mostafa2080 <mostafashaaban2080@gmail.com>
2023-03-18 02:24:05 +0200

    comment fot adminstrator and books Controller added

--- Controllers/BooksController.js
+++ Controllers/BooksController.js
@@ -1,111 +1,114 @@
-const mongoose = require("mongoose");
-require("../Model/BooksModel");
-const Books = mongoose.model("books");
+const mongoose = require('mongoose');
+require('../Model/BooksModel');
+const Books = mongoose.model('books');
 
 exports.getAllBooks = async (req, res, next) => {
-    try {
-        const books = await Books.find({});
-        res.status(200).json({ books });
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const books = await Books.find({});
+    res.status(200).json({ books });
+  } catch (err) {
+    next(err);
+  }
 };
 
+// This function gets a book by its ID from the database and returns it as a JSON object.
+// If the book is not found, it throws an error with message "Book not found 💩".
+// If there's an error in fetching the book, it calls the error handling middleware.
 exports.getBook = async (req, res, next) => {
-    try {
-        const book = await Books.findById(req.params._id);
-        if (!book) next(new Error("Book not found 💩"));
-        else {
-            res.status(200).json({ book });
-        }
-    } catch (err) {
-        next(err);
+  try {
+    const book = await Books.findById(req.params._id);
+    if (!book) next(new Error('Book not found 💩'));
+    else {
+      res.status(200).json({ book });
     }
+  } catch (err) {
+    next(err);
+  }
 };
-
+//add book to db
 exports.addBook = async (req, res, next) => {
-    try {
-        const book = await new Books({
-            title: req.body.title,
-            author: req.body.author,
-            publisher: req.body.publisher,
-            dateAdded: req.body.dateAdded,
-            datePublished: req.body.datePublished,
-            category: req.body.category,
-            pagesCount: req.body.pagesCount,
-            copiesCount: req.body.copiesCount,
-            isAvailable: req.body.isAvailable,
-            shelfNo: req.body.shelfNo,
-        }).save();
+  try {
+    const book = await new Books({
+      title: req.body.title,
+      author: req.body.author,
+      publisher: req.body.publisher,
+      dateAdded: req.body.dateAdded,
+      datePublished: req.body.datePublished,
+      category: req.body.category,
+      pagesCount: req.body.pagesCount,
+      copiesCount: req.body.copiesCount,
+      isAvailable: req.body.isAvailable,
+      shelfNo: req.body.shelfNo,
+    }).save();
 
-        res.status(200).json({ book });
-    } catch (err) {
-        next(err);
-    }
+    res.status(200).json({ book });
+  } catch (err) {
+    next(err);
+  }
 };
 
+//update a Book
 exports.updateBook = async (req, res, next) => {
-    try {
-        const book = await Books.updateOne(
-            { _id: req.params._id },
-            {
-                $set: {
-                    title: req.body.title,
-                    author: req.body.author,
-                    publisher: req.body.publisher,
-                    dateAdded: req.body.dateAdded,
-                    datePublished: req.body.datePublished,
-                    category: req.body.category,
-                    pagesCount: req.body.pagesCount,
-                    copiesCount: req.body.copiesCount,
-                    isAvailable: req.body.isAvailable,
-                    shelfNo: req.body.shelfNo,
-                },
-            }
-        );
+  try {
+    const book = await Books.updateOne(
+      { _id: req.params._id },
+      {
+        $set: {
+          title: req.body.title,
+          author: req.body.author,
+          publisher: req.body.publisher,
+          dateAdded: req.body.dateAdded,
+          datePublished: req.body.datePublished,
+          category: req.body.category,
+          pagesCount: req.body.pagesCount,
+          copiesCount: req.body.copiesCount,
+          isAvailable: req.body.isAvailable,
+          shelfNo: req.body.shelfNo,
+        },
+      }
+    );
 
-        res.status(200).json({ book });
-    } catch (err) {
-        next(err);
-    }
+    res.status(200).json({ book });
+  } catch (err) {
+    next(err);
+  }
 };
-
+//Delete A Book
 exports.deleteBook = async (req, res, next) => {
-    if (
-        !(
-            req.decodedToken.role == "Admin" ||
-            req.decodedToken.role == "BasicAdmin"
-        )
-    ) {
-        res.status(401).json({ message: "Unauthorized" });
-        return;
+  if (
+    !(req.decodedToken.role == 'Admin' || req.decodedToken.role == 'BasicAdmin')
+  ) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return;
+  }
+  try {
+    const book = await Books.deleteOne({ _id: req.params._id });
+    if (book.deletedCount == 0) next(new Error('Book not found 💩'));
+    else {
+      res.status(200).json({ book });
     }
-    try {
-        const book = await Books.deleteOne({ _id: req.params._id });
-        if (book.deletedCount == 0) next(new Error("Book not found 💩"));
-        else {
-            res.status(200).json({ book });
-        }
-    } catch (err) {
-        next(err);
-    }
+  } catch (err) {
+    next(err);
+  }
 };
 
+//Find Books That match Certain keyword
+// $or operator is used to search for books that have a title, author, or publisher that matches the keyword.
 exports.searchBooks = async (req, res, next) => {
-    try {
-        const searchResultbooks = await Books.find({
-            $or: [
-                { title: { $regex: req.params.keyword } },
-                { author: { $regex: req.params.keyword } },
-                { publisher: { $regex: req.params.keyword } },
-            ],
-        });
-        if (searchResultbooks.length === 0) {
-            res.status(404).json({ message: "No books found" });
-        } else {
-            res.status(200).json({ searchResultbooks });
-        }
-    } catch (err) {
-        next(err);
+  try {
+    const searchResultbooks = await Books.find({
+      $or: [
+        { title: { $regex: req.params.keyword } },
+        { author: { $regex: req.params.keyword } },
+        { publisher: { $regex: req.params.keyword } },
+      ],
+    });
+    if (searchResultbooks.length === 0) {
+      res.status(404).json({ message: 'No books found' });
+    } else {
+      res.status(200).json({ searchResultbooks });
     }
+  } catch (err) {
+    next(err);
+  }
 };
